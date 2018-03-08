a151734553b9fd57cbc37cb9949fc890c459c9e4
kkudumu <kkudumu@gmail.com>
2018-03-08 11:34:08 -0800

    Completed Simple Todo App

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 //import stuff
 import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 
 //create stuff
-class App extends React.Component{
+class App extends React.Component {
   state = {
     text: "",
     todo: []
@@ -12,54 +12,75 @@ class App extends React.Component{
     var newTodo = this.state.text;
     var arr = this.state.todo;
     arr.push(newTodo);
-    this.setState({todo: arr, text: ""})
+    this.setState({ todo: arr, text: "" })
   }
-  deleteTodo = (t) =>{
+  deleteTodo = (t) => {
     var arr = this.state.todo;
     var pos = arr.indexOf(t);
     arr.splice(pos, 1);
-    this.setState({todo: arr});
+    this.setState({ todo: arr });
   }
-  renderTodos= () => {
-    return this.state.todo.map(t=>{
+  renderTodos = () => {
+    return this.state.todo.map(t => {
       return (
-        <Text 
-        key={t}
-        onPress={()=>{this.deleteTodo(t)}}
+        <TouchableOpacity  key={t}>
+        <Text
+        style ={styles.todo}
+          onPress={() => { this.deleteTodo(t) }}
         >{t}</Text>
+        </TouchableOpacity>
       )
     })
   }
-  render(){
-    return(
-      <View style={styles.viewStyle}>
-        <Text> Simple Todo App </Text>
-        <TextInput 
-        style = {styles.inputStyle}
-        onChangeText={(text)=>this.setState({text})}
-        value = {this.state.text}
-        />
-        <Button 
-        title="Add Todo"
-        onPress={this.addTodo}
-        />
-        {this.renderTodos()}
+  render() {
+  
+    return (
+      <View style={styles.wholeStyle}>
+        <View style={styles.viewStyle}>
+          <Text style={styles.header}> Simple Todo App </Text>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+          />
+          <Button
+            title="Add Todo"
+            color="white"
+            onPress={this.addTodo}
+          />
+          <View style={{marginTop: 100}}/>
+          {this.renderTodos()}
         </View>
+      </View>
 
     )
   }
 }
 
 const styles = {
+  wholeStyle: {
+    backgroundColor: "#0288D1",
+    flex: 1
+  },
   viewStyle: {
-  flex: 1,
-  alignItems: 'center', 
-  justifyContent: 'center'
-},
-  inputStyle:{
-  height: 40,
-  borderColor: "green",
-  borderWidth: 1
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  inputStyle: {
+    height: 40,
+    borderColor: "white",
+    borderWidth: 1
+  },
+  header: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  todo: {
+    fontSize: 24,
+    color: 'white'
   }
 }
 
